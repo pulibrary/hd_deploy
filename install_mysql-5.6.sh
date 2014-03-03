3b3425e1fe861e1f5f2361458e95d2cafc3415bd
Alicia Cozine <cozi@visi.com>
2014-03-03 14:00:50 -0600

    headless mysql

--- install_mysql-5.6.sh
+++ install_mysql-5.6.sh
@@ -6,10 +6,8 @@ if [ "$EUID" -ne "0" ] ; then
         exit 1
 fi
 
-apt-get install mysql-server -y
-
-# set mysql to start automatically on reboot
-#chkconfig mysqld on
+export DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install mysql-server
 
 # move mysql to /opt
 #cp -a /var/lib/mysql/ /opt/
@@ -25,6 +23,6 @@ default-storage-engine = InnoDB' -i.bak /etc/mysql/my.cnf
 
 # start mysql and run through the secure installation
 #sudo service mysql stop
-service mysql start
+#service mysql start
 /usr/bin/mysql_secure_installation < /vagrant/mysql_secure_non-secure_defaults.txt
 mysql -u root -ppassword < /vagrant/mysql_fedora_tables.txt 
