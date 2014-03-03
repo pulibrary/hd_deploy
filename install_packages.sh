a36a299e0e2c9a05dcce4fe8d932017eac8bed24
Alicia Cozine <cozi@visi.com>
2014-03-03 11:08:13 -0600

    add -y switch

--- install_packages.sh
+++ install_packages.sh
@@ -10,7 +10,7 @@ fi
 apt-get update -y && apt-get upgrade -y
 
 # install other necessary libraries
-apt-get install build-essential git git-core curl openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config libmagickwand-dev imagemagick libcurl4-openssl-dev apache2-prefork-dev libxvidcore-dev
+apt-get install build-essential git git-core curl openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config libmagickwand-dev imagemagick libcurl4-openssl-dev apache2-prefork-dev libxvidcore-dev -y
 
 # expect is necessary for fedora install script
 apt-get install expect -y
