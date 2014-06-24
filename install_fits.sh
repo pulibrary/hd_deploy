f1d5ed81a34d835c228791a50295a5c6e794d985
Jon Stroop <jpstroop@gmail.com>
2014-06-24 09:36:56 -0400

    Latest fits

--- install_fits.sh
+++ install_fits.sh
@@ -1,7 +1,7 @@
 mkdir -p /opt/install && cd /opt/install
-wget http://fits.googlecode.com/files/fits-0.6.2.zip
-unzip fits-0.6.2.zip  
-sudo chmod a+x fits-0.6.2/fits.sh  
-sudo cp -r fits-0.6.2/* /usr/local/bin/
+wget http://projects.iq.harvard.edu/files/fits/files/fits-0.8.0.zip
+unzip fits-0.8.0.zip  
+sudo chmod a+x fits-0.8.0/fits.sh  
+sudo cp -r fits-0.8.0/* /usr/local/bin/
 sudo ln -s /usr/local/bin/fits.sh /usr/local/bin/fits
 
